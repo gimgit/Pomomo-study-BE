6d3d2f07631f3ed0cef3c4fd83fc9793d7a69452
heekyung <hshsnamu@nate.com>
2021-12-27 12:03:24 +0900

    Style : usePrettier

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,13 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 app.use(cors());
 
+const Router = require("./routes/router");
+app.use("/api", Router);
 
-const Router = require('./routes/router')
-app.use('/api', Router)
-
-app.use(express.urlencoded({extended : true}))
-app.use('/public', express.static('public'))
-
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static("public"));
 
 module.exports = app;
