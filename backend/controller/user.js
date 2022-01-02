e0ca0a870dc07145f41ef60eeabbe82b72cf24cb
heekyung <hshsnamu@nate.com>
2022-01-03 01:41:21 +0900

    fix/mypage

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -2,19 +2,33 @@ const { User, StudyTime, sequelize } = require("../models");
 const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
-async function checkUserInfo(req, res) {
-  let now = new Date(Date.now() + 32400000);
-  let year = now.getFullYear();
-  let month = now.getMonth() + 1;
-  let date = now.getDate();
-  let startDate = now.getDate() - 1;
-  let todayStart = `${year}-${month}-${startDate}T04:00:00.000Z`;
-  let todayEnd = `${year}-${month}-${date}T04:00:00.000Z`;
-  console.log(now);
-  console.log(todayStart);
-  console.log(todayEnd);
+//1~3sequelize check
 
-  const { userId } = req.params;
+let [today, tomorrow, yesterday] = [
+  new Date(Date.now() + 9 * 60 * 60 * 1000),
+  new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+  new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
+];
+let [year, month, date] = [
+  today.getFullYear(),
+  today.getMonth() + 1,
+  today.getDate(),
+];
+let [nextDate, dayBefore] = [tomorrow.getDate(), yesterday.getDate()];
+
+async function checkUserInfo(req, res) {
+  const userId = res.locals.user.userId;
+  let todayStart;
+  let todayEnd;
+
+  let isDawn = new Date().getHours();
+  isDawn < 4
+    ? (todayStart = `${year}-${month}-${dayBefore}T04:00:00.000Z`)
+    : (todayStart = `${year}-${month}-${date}T04:00:00.000Z`);
+  isDawn < 4
+    ? (todayEnd = `${year}-${month}-${date}T04:00:00.000Z`)
+    : (todayEnd = `${year}-${month}-${nextDate}T04:00:00.000Z`);
+  // 4시를 기점으로 오늘 공부시간 가져오는 기준일자 달라짐
 
   try {
     const userInfo = await User.findAll({
@@ -34,6 +48,9 @@ async function checkUserInfo(req, res) {
       },
       attributes: [[sequelize.fn("sum", sequelize.col("studyTime")), "today"]],
     });
+
+    console.log(todayStart);
+    console.log(todayEnd);
     return res.status(200).json({
       user: userInfo,
       totalRecord: studyRecord,
@@ -50,30 +67,21 @@ async function updateUserInfo(req, res) {
   const { userId } = req.params;
   const { category, nick } = req.body;
   try {
-    const existNick = await User.findOne({
-      where: { nick: nick },
-    });
-    if (existNick)
-      return res
-        .status(400)
-        .send({ errorMessage: "이미 사용중인 닉네임입니다." });
     const userInfo = await User.findOne({
       where: { userId: userId },
     });
     if (!userInfo) return res.status(400).send("err");
-
-    await User.update(
-      { category: category, nick: nick },
-      { where: { userId: userId } }
-    );
+    let userNewInfo = {};
+    if (nick) userNewInfo.nick = nick;
+    if (category) userNewInfo.category = category;
+    await User.update(userNewInfo, { where: { userId: userId } });
     return res.status(201).send({ msg: "수정완료!" });
   } catch (err) {
     return res.status(400).send({
-      msg: "요청한 데이터 형식이 올바르지 않습니다.",
+      msg: "이미 존재하는 닉네임 또는 요청하는 데이터 형식이 올바르지 않습니다.",
     });
   }
 }
-
 async function updateUserStatus(req, res) {
   const { userId } = req.params;
   const { statusMsg } = req.body;
