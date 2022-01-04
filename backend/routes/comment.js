2a408162f4ddd87177b0eb962cba790d0e4067f6
hyeop <lsh6166@gmail.com>
2022-01-04 10:19:32 +0900

    ifx comment

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -3,11 +3,7 @@ const router = require("express").Router();
 const commentCtl = require("../controller/comment");
 const authorization = require("../middlewares/auth-middlewares.js");
 
-router.post("/:postId/comments", authorization, commentCtl.postComment);
-router.delete(
-  "/:postId/comments/:commentId",
-  authorization,
-  commentCtl.deleteComment
-);
+router.post("/", authorization, commentCtl.postComment);
+router.delete("/:commentId", authorization, commentCtl.deleteComment);
 
 module.exports = router;
